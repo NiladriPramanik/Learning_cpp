ea8f9b0287a4abd3b16cda761995762f586a8e32
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-02-17 10:34:02 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a,b;
+    cout<<"Enter first number"<<endl;
+    cin>>a;
+    cout<<"Enter secom=nd number"<<endl;
+    cin>>b;
+    cout<<"The sum is :"<<a+b;
+    return 0;
+}
