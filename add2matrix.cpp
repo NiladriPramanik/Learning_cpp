76591df2db9da1293acd5ac0332f0fe5a2245c0a
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-05-30 23:57:32 +0530

    Create add2matrix.cpp

---
+++ add2matrix.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix1[2][2], matrix2[2][2], sum[2][2];
+
+    cout << "Enter the elements of matrix 1:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> matrix1[i][j];
+        }
+    }
+
+    cout << "Enter the elements of matrix 2:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> matrix2[i][j];
+        }
+    }
+
+    // Perform matrix addition
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    cout << "Sum of the matrices:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
