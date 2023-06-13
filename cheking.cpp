c7c2c942f0f6ff57de7b1e29f25c1bd5656c9960
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-06-13 22:39:04 +0530

    Create cheking.cpp

---
+++ cheking.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+char a;
+cout<<"enter the value of a"<<endl;
+cin>>a;
+
+if(a>='a' && a<='z' )
+{
+    cout<<"lower case alphabet"<<endl;
+}
+else if (a>='A' && a<='Z')
+{
+    cout<<"upper case alphabet"<<endl;
+}
+
+
+if (a>='0' && a<='9')
+
+    {
+cout<<"numeric"<<endl;
+}
+
+else
+{
+cout<<"invalid input";    
+}
+}
