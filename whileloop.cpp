a9533847644461a19a4d603d9de3259e9fcaf74f
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-06-14 22:30:11 +0530

    Create whileloop.cpp

---
+++ whileloop.cpp
@@ -0,0 +1,17 @@
+//simple code using while loop
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cout<<"enetr the value of n"<<endl;
+    cin>>n;
+while(n<=10)
+{
+    int i=1;
+    cout<<n<<" "<<endl;
+    i=+1;
+}
+
+}
