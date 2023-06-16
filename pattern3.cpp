1c52706fc4853f1792539c9d93caa946ee9ef189
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-06-16 18:31:51 +0530

    Create pattern3.cpp

---
+++ pattern3.cpp
@@ -0,0 +1,28 @@
+//pattern 4321
+//        4321
+//        4321
+//        4321
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i=1;
+
+    while(i<=n)
+    {
+       int j=1;
+       while(j<=n)
+       {
+        int a=n-j+1;
+        cout<<a;
+        j=j+1;
+       }
+       cout<<endl;
+       i=i+1;
+
+    }
+}
