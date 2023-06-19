75cf1641c3fcc13591df4e2590f57b347d723072
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-06-20 04:12:42 +0530

    Update countingbinary.cpp

--- countingbinary.cpp
+++ countingbinary.cpp
@@ -1,13 +1,13 @@
 //code for counting no. of 1s in binary of given integer
 #include<iostream>
-#include<bitset>
+
 using namespace std;
 int main ()
 {
     int n ;
     cout<<"enter the number"<<endl;
     cin>>n;
-    bitset<32>binary(n); //converting int to binary
+    
     int count = 0 ;
     while(n!=0) {
         //cheking last bit
